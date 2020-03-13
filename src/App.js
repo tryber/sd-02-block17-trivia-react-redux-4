f867a31db90989a0c4f0fa4d458611cdcb74220f
Andrey <andreytsuzuki@gmail.com>
2020-03-13 19:10:18 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-
-import Feedback from './Pages/Feedback';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './Pages/Login';
+import Game from './Pages/Game';
+import FeedBack from './Pages/Feedback';
+import Configuracao from './Pages/Configuracao';
+import Ranking from './Pages/Ranking';
 
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <Feedback />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/game-page" component={Game} />
+          <Route exact path="/game-feedback" component={FeedBack} />
+          <Route exact path="/game-configuration" component={Configuracao} />
+          <Route exact path="/game-ranking" component={Ranking} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
