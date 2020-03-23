63fe671edd6ae6fa43f65ee9c80eed437ba0cd52
Giorgia <giorgialeoniac@gmail.com>
2020-03-23 16:40:23 -0300

    Ainda falta um pouco

--- src/Pages/Login/components/Campo.js
+++ src/Pages/Login/components/Campo.js
@@ -1,20 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-
+import { Link } from 'react-router-dom';
+// import settingsBtn from '../../../imgs/settings.png'
 import './style.css';
 
-export default class Campo extends Component {
-  render() {
-    const { name, type } = this.props;
+class Campo extends React.Component {
+  renderSettingsButton() {
+    return (
+      <Link to="/settings">
+      <button className="settingsBtn">
+        Teste
+        {/* <img className="settingsIcon" src={settingsBtn} alt="settings icon" /> */}
+      </button>
+      </Link>
+    )
+  }
+
+  renderLoginSection() {
     return (
       <div className="login_campo">
-        <p>{name}</p>
-        <input type={type} />
+        <label htmlFor="email">E-mail do Gravatar:</label>
+        <input
+          id="email"
+          name="email"
+          type="text"
+          placeholder="Insira seu e-mail Gravatar"
+        />
+        <label htmlFor="name">Nome do Jogador:</label>
+        <input
+          id="name"
+          name="name"
+          type="name"
+          placeholder="Insira seu nome"
+        />
+        <button className="btn-jogar">JOGAR!</button>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="whole-page">
+        <div className="settingsDiv">
+          {this.renderSettingsButton()}
+        </div>
+        {this.renderLoginSection()}
       </div>
     );
   }
 }
 
+export default Campo;
 
 Campo.propTypes = {
   name: PropTypes.string.isRequired,
--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Campo from './components/Campo';
+import Campo from "./components/Campo";
 
 import './style.css';
 
@@ -9,9 +9,7 @@ export default class Login extends Component {
     return (
       <div className="page_login">
         <div className="content">
-          <Campo name={'Email do Gravatar:'} type={'email'} />
-          <Campo name={'Nome do jogador:'} type={'text'} />
-          <button type="button">JOGAR!</button>
+          <Campo />
         </div>
       </div>
     );
