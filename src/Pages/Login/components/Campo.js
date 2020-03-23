bc4fffb53bfc54be734ef85f9d50ca88f685ad2d
Giorgia <giorgialeoniac@gmail.com>
2020-03-23 17:07:03 -0300

    CC

--- src/Pages/Login/components/Campo.js
+++ src/Pages/Login/components/Campo.js
@@ -1,49 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import settingsBtn from '../../../imgs/settings.png'
+import settingsBtn from '../../../imgs/settings.png';
 import './style.css';
 
+
+const renderLoginSection = () => (
+  <div className="login_campo">
+    <label htmlFor="email">E-mail do Gravatar:</label>
+    <input
+      id="email"
+      name="email"
+      type="text"
+      placeholder="Insira seu e-mail Gravatar"
+    />
+    <label htmlFor="name">Nome do Jogador:</label>
+    <input
+      id="name"
+      name="name"
+      type="name"
+      placeholder="Insira seu nome"
+    />
+    <button className="btn-jogar">JOGAR!</button>
+  </div>
+);
+
+const renderSettingsButton = () => (
+  <Link to="/settings">
+    <icon className="settingsBtn">
+      <img className="settingsIcon" src={settingsBtn} alt="settings icon" />
+    </icon>
+  </Link>
+);
+
 class Campo extends React.Component {
-  renderSettingsButton() {
-    return (
-      <Link to="/settings">
-      <icon className="settingsBtn">
-        <img className="settingsIcon" src={settingsBtn} alt="settings icon" />
-      </icon>
-      </Link>
-    )
-  }
-
-  renderLoginSection() {
-    return (
-      <div className="login_campo">
-        <label htmlFor="email">E-mail do Gravatar:</label>
-        <input
-          id="email"
-          name="email"
-          type="text"
-          placeholder="Insira seu e-mail Gravatar"
-        />
-        <label htmlFor="name">Nome do Jogador:</label>
-        <input
-          id="name"
-          name="name"
-          type="name"
-          placeholder="Insira seu nome"
-        />
-        <button className="btn-jogar">JOGAR!</button>
-      </div>
-    );
-  }
-
   render() {
     return (
       <div className="whole-page">
         <div className="settingsDiv">
-          {this.renderSettingsButton()}
+          {renderSettingsButton()}
         </div>
-        {this.renderLoginSection()}
+        {renderLoginSection()}
       </div>
     );
   }
