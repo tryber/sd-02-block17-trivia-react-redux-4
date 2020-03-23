87229fa93f8513a2b635320954b11f8bb4eba291
Andrey <andreytsuzuki@gmail.com>
2020-03-23 18:47:04 -0300

    coisa

--- src/Pages/Feedback/index.js
+++ src/Pages/Feedback/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import PropTypes from "prop-types";
 import './style.css';
 
 const propriedade = {
@@ -7,19 +7,34 @@ const propriedade = {
   acertos: 2,
 };
 
+function situacaoRender(acertos) {
+  return (
+    (acertos < 3) ?
+    <h3>Podia ser melhor...</h3> :
+    <h3>Mandou bem !</h3>
+  );
+}
+
+function headerRender() {
+  const { acertos, jogador } = propriedade;
+  return (
+    <div className="header">
+      <p>Jogador: {jogador}</p>
+      <div className="pontos">
+        <p>Pontos: {acertos * 10}</p>
+        <i className="material-icons">
+          fiber_manual_record
+        </i>
+      </div>
+    </div>
+  );
+}
+
 export default class Feedback extends Component {
   constructor(props) {
     super(props);
     this.redirectRanking = this.redirectRanking.bind(this);
     this.redirectGame = this.redirectGame.bind(this);
-  }
-
-  situacaoRender(acertos) {
-    return(
-      (acertos < 3) ?
-      <h3>Podia ser melhor...</h3> :
-      <h3>Mandou bem !</h3>
-    );
   }
 
   redirectGame() {
@@ -30,26 +45,11 @@ export default class Feedback extends Component {
     this.props.history.push('/game-ranking');
   }
 
-  headerRender() {
-    const { acertos, jogador } = propriedade;
-    return (
-      <div className="header">
-        <p>Jogador: {jogador}</p>
-        <div className="pontos">
-          <p>Pontos: {acertos * 10}</p>
-          <i className="material-icons">
-            fiber_manual_record
-          </i>
-        </div>
-      </div>
-    );
-  }
-
   bodyRender() {
     const { acertos } = propriedade;
     return (
       <div className="body">
-        {this.situacaoRender(acertos)}
+        {situacaoRender(acertos)}
         <div>
           <p>Você acertou {acertos} questões</p>
           <p>Um total de {acertos * 10} pontos</p>
@@ -76,10 +76,14 @@ export default class Feedback extends Component {
     return (
       <div className="page_feedback">
         <div className="content">
-          {this.headerRender()}
+          {headerRender()}
           {this.bodyRender()}
         </div>
       </div>
     );
   }
 }
+
+history: PropTypes.shape({
+  push: PropTypes.func
+}).isRequired
