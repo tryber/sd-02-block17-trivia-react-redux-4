24be284679ec73484465902737291f8a34e36800
Giorgia <giorgialeoniac@gmail.com>
2020-03-27 15:26:23 -0300

    Iniciando o gerador de ranking

--- src/Pages/Ranking/RankingGenerator.js
+++ src/Pages/Ranking/RankingGenerator.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
-export default class RankingGenerator extends Component {
+// Deve-se mostrar uma lista com a imagem de perfil vinda do Gravatar, nome e pontuação das pessoas que jogaram em ordem decrescente (da maior pontuação para a menor);
+// O ranking deve ser armazenado no navegador através do localStorage.
+
+class RankingGenerator extends Component {
+  getLocalRanking() {
+    let getRanking = localStorage.getItem();
+  }
+
   render() {
     return (
       <div>TESTE</div>
--- src/Pages/Ranking/index.js
+++ src/Pages/Ranking/index.js
@@ -10,3 +10,4 @@ export default class Ranking extends Component {
     );
   }
 }
+
