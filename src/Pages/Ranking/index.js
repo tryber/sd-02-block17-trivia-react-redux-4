d6f65e75b8d23542bf9ad0a9f6d9bbde282bd9d0
Giorgia <giorgialeoniac@gmail.com>
2020-03-27 16:54:57 -0300

    Iniciado componente Ranking

--- src/Pages/Ranking/index.js
+++ src/Pages/Ranking/index.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import './index.css';
+
+// Deve-se mostrar uma lista com a imagem de perfil vinda do Gravatar, nome e pontuação das pessoas que jogaram em ordem decrescente (da maior pontuação para a menor);
+// O ranking deve ser armazenado no navegador através do localStorage.
 
 export default class Ranking extends Component {
+  // getLocalRanking() {
+  //   let getRanking = localStorage.getItem();
+  // }
+
   render() {
     return (
       <div>
-        <h3>Ranking</h3>
-        <RankingGenerator />
+        <h3 className="page-title">Ranking</h3>
+        <div className="ranking-results">
+          TESTE
+        </div>
       </div>
     );
   }
 }
+
 
