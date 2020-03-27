bc4b053a13bdacba52089973aeeb722993f25fec
Giorgia <giorgialeoniac@gmail.com>
2020-03-27 17:06:44 -0300

    CC

--- src/Pages/Ranking/index.js
+++ src/Pages/Ranking/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './index.css';
 
-// Deve-se mostrar uma lista com a imagem de perfil vinda do Gravatar, 
-//nome e pontuação das pessoas que jogaram em ordem decrescente 
-//(da maior pontuação para a menor);
+// Deve-se mostrar uma lista com a imagem de perfil vinda do Gravatar,
+// nome e pontuação das pessoas que jogaram em ordem decrescente
+// (da maior pontuação para a menor);
 // O ranking deve ser armazenado no navegador através do localStorage.
 
 export default class Ranking extends Component {
